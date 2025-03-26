5178ab85f639a8539c666d5c6d8e7e2315075429
Destroyer457735 <alec.mckeever@gmail.com>
2025-03-25 17:19:41 -0700

    Update CatWorker_Tex_jsList.js

--- CatWorker_Tex/CatWorker_Tex_jsList.js
+++ CatWorker_Tex/CatWorker_Tex_jsList.js
@@ -1,1 +1,1 @@
-["idle","idle-","dumping","dumping-","mining position","run-01","run-02","run-03","run-04","run-05","run-06","run-07","run-08","run-09","run-10","run-11","run-12","run-13","run-14","run-15","run-16","run--01","run--02","run--03","run--04","run--05","run--06","run--07","run--08","run--09","run--10","run--11","run--12","run--13","run--14","run--15","run--16","shadows"]
+["idle","dumping","mining position","run-01","run-02","run-03","run-04","run-05","run-06","run-07","run-08","run-09","run-10","run-11","run-12","run-13","run-14","run-15","run-16","shadows"]
